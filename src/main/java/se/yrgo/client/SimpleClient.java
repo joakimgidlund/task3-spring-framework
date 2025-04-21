6c656e4ac06cea16c339155e7e692896e8266a5e
joakimgidlund <jockegidlund@gmail.com>
2025-04-21 18:48:16 +0200

    CustomerManagementService bean tested

--- src/main/java/se/yrgo/client/SimpleClient.java
+++ src/main/java/se/yrgo/client/SimpleClient.java
@@ -1,10 +1,18 @@
 package se.yrgo.client;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import se.yrgo.services.customers.CustomerManagementService;
 
 public class SimpleClient {
 
     public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
 
+        CustomerManagementService customerManagementService = context.getBean("customerManagementService", CustomerManagementService.class);
+
+        customerManagementService.getAllCustomers().forEach(System.out::println);
+
+        context.close();
     }
 }
