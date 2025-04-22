7da0ccb2a57a622cb682a25aaff9b39a04f1ec70
joakimgidlund <jockegidlund@gmail.com>
2025-04-22 13:05:10 +0200

    added test functionality

--- src/main/java/se/yrgo/client/SimpleClient.java
+++ src/main/java/se/yrgo/client/SimpleClient.java
@@ -17,11 +17,15 @@ public class SimpleClient {
     public static void main(String[] args) throws CustomerNotFoundException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
 
-        CustomerManagementService customerManagementService = context.getBean("customerManagementService", CustomerManagementService.class);
+        CustomerManagementService customerManagementService = context.getBean("customerManagementService",
+                CustomerManagementService.class);
+
+        // Print every customer
         customerManagementService.getAllCustomers().forEach(System.out::println);
 
         CallHandlingService callHandlingService = context.getBean("callHandlingService", CallHandlingService.class);
 
+        // Get a customer that exists to record a call in CallHandlingService
         Customer customer = customerManagementService.getAllCustomers().get(0);
 
         Call call = new Call("test call");
@@ -30,6 +34,10 @@ public class SimpleClient {
         actionColl.add(new Action("make tests", null, "testuser"));
 
         callHandlingService.recordCall(customer.getCustomerId(), call, actionColl);
+
+        // Print the calls of "customer" to make sure the new call was added
+        customerManagementService.getFullCustomerDetail(customer.getCustomerId()).getCalls()
+                .forEach(System.out::println);
 
         context.close();
     }
